9dbb081cd763f737f2a0c9cadc33adf6f3f0788a
Victor Magalhaes <victor.barreto@buser.com.br>
2022-06-30 12:18:23 -0300

    Template estendido

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,5 +3,6 @@ from . import views
 import cursos
 
 urlpatterns = [
-    path("", views.pagina_inicial)
+    path("", views.pagina_inicial, name='cursos_inicio'),
+    path("listar/meus_cursos/", views.listar_cursos, name='meus_cursos'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def pagina_inicial(request):
     return render(request, "cursos/pagina_inicial.html")
+
+def listar_cursos(request):
+    return render(request, "cursos/listar_cursos.html")
